977d37066a3b5266336934c740c07b1993a34aa0
dalakoti07 <saurabhdalakoti07@gmail.com>
2020-02-11 20:42:37 +0530

    initial commit

---
+++ app/src/main/java/com/example/feedmememes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.feedmememes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
